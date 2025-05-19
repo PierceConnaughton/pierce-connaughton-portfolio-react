edc60b21c3ed68100f770b7a74f7b07abf9cd035
PierceConnaughton <pierce.connaught@gmail.com>
2025-05-19 21:10:45 +0100

    Update Vercel Analytics import to use React version

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header.tsx';
 import Footer from './components/Footer/Footer.tsx';
 import { Container } from '@mui/material';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const App: React.FC = () => {
     return (
